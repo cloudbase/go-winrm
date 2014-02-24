d97196a9fa7fa854424f27eee8ca92e5b01be2c3
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2014-02-24 14:04:10 +0200

    Some refactor

--- transport.go
+++ transport.go
@@ -6,36 +6,61 @@ import (
     "crypto/tls"
     "fmt"
     "bytes"
-    // "errors"
+    "errors"
 )
 
-func GetHttpHeader () (map[string]string) {
+type CertificateCredentials struct {
+    Cert    string
+    Key     string
+    CA      string
+}
+
+type SoapRequest struct {
+    Endpoint        string
+    AuthType        string
+    Username        string
+    Passwd          string
+    HttpInsecure    bool
+    CertAuth        *CertificateCredentials
+}
+
+func (conf *SoapRequest) SendMessage(data []byte) (*http.Response, error){
+    if conf.AuthType == "BasicAuth"{
+        if conf.Username == "" || conf.Passwd == "" {
+            fmt.Errorf("AuthType BasicAuth needs Username and Passwd")
+        }
+        return conf.HttpBasicAuth(data)
+    }
+    return nil, nil
+}
+
+func (conf *SoapRequest) GetHttpHeader () (map[string]string) {
     header := make(map[string]string)
     header["Content-Type"] = "application/soap+xml;charset=UTF-8"
     header["User-Agent"] = "Python WinRM client"
     return header
 }
 
-func HttpBasicAuth (url, username, pass string, data []byte, insecure bool) (*http.Response, error){
-    protocol := strings.Split(url, ":")
+func (conf *SoapRequest) HttpBasicAuth (data []byte) (*http.Response, error){
+    protocol := strings.Split(conf.Endpoint, ":")
     if protocol[0] != "http" && protocol[0] != "https"{
-        fmt.Errorf("Invalid protocol. Expected http or https")
+        return nil, errors.New("Invalid protocol. Expected http or https")
     }
 
-    header := GetHttpHeader()
+    header := conf.GetHttpHeader()
     
     client := &http.Client{}
     // Ignore SSL certificate errors
     if protocol[0] == "https" {
         tr := &http.Transport{
-            TLSClientConfig: &tls.Config{InsecureSkipVerify: insecure},
+            TLSClientConfig: &tls.Config{InsecureSkipVerify: conf.HttpInsecure},
         }
         client.Transport = tr
     }
     body := bytes.NewBuffer(data)
-    req, err := http.NewRequest("POST", url, body)
+    req, err := http.NewRequest("POST", conf.Endpoint, body)
     req.ContentLength = int64(len(data))
-    req.SetBasicAuth(username, pass)
+    req.SetBasicAuth(conf.Username, conf.Passwd)
 
     for k, v := range header {
         req.Header.Add(k, v)
