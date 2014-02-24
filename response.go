64b6df2f3b248754ba9a88ab0457ee429ae3d6f1
Robert Tingirica <rtingirica@cloudbasesolutions.com>
2014-02-24 23:48:08 +0200

    Added more xml handling and getting stdout stderr and exitcode

--- response.go
+++ response.go
@@ -2,8 +2,11 @@ package winrm
 
 import (
 	"encoding/xml"
+    "fmt"
+    "bytes"
     "io"
     "io/ioutil"
+    "encoding/base64"
 )
 
 type ResponseSelector struct{
@@ -32,9 +35,13 @@ type ResponseHeader struct{
     RelatesTo   string      `xml"a:RelatesTo"`
 }
 
+type CommandResponse struct{
+    CommandId   string  `xml"rsp:CommandId"`
+}
+
 type ResponseShell struct{
     // xmlnsRsp        string  `xml:"xmlns:rsp,attr"`
-    ShellId            string  `xml"rsp:ShellId"`
+    ShellId            string  `xml"rsp:ShellId`
     ResourceUri        string  `xml"rsp:ResourceUri"`
     Owner              string  `xml"rsp:Owner"`
     ClientIP           string  `xml"rsp:ClientIP"`
@@ -45,24 +52,42 @@ type ResponseShell struct{
     ShellInactivity    string  `xml"rsp:OutputStreams"`
 }
 
+type ResponseStream struct{
+    Value       string  `xml:",innerxml"`
+    Name        string  `xml:"Name,attr"`
+    End         string  `xml:"End,attr"`
+}
+
+type ResponseCommandState struct{
+    ExitCode        int     `xml"rsp:ExitCode"`
+    CommandId       string  `xml:"CommandId,attr"`
+    State           string  `xml:"State,attr"`
+}
+
+type ReceiveResponse struct{
+    Stream          []ResponseStream        `xml"rsp:Stream"`
+    CommandState    *ResponseCommandState   `xml"rsp:CommandState"`
+}
+
 type ResponseBody struct{
-    ResourceCreated *ResourceCreated   `xml"x:ResourceCreated"`
-    Shell           *ResponseShell     `xml"rsp:Shell"`
+    CommandResponse     *CommandResponse   `xml"rsp:CommandResponse"`
+    ResourceCreated     *ResourceCreated   `xml"x:ResourceCreated"`
+    Shell               *ResponseShell     `xml"rsp:Shell"`
+    ReceiveResponse     *ReceiveResponse   `xml"rsp:ReceiveResponse"`
 }
 
 type ResponseEnvelope struct{
-    XMLName     xml.Name `xml"s:Envelope"`
-    xmlnsS      string   `xml"xmlns:s,attr"`
-    // xmlnsA      string   `xml"xmlns:a,attr"`
-    // xmlnsX      string   `xml"xmlns:x,attr"`
-    // xmlnsW      string   `xml"xmlns:w,attr"`
-    // xmlnsRsp    string   `xml"xmlns:rsp,attr"`
-    // xmlnsP      string   `xml"xmlns:p,attr"`
-    // xmlnsLang   string   `xml"xmlns:lang,attr"`
+    XMLName     xml.Name        `xml"s:Envelope"`
+    // xmlnsS      string       `xml"xmlns:s,attr"`
+    // xmlnsA      string       `xml"xmlns:a,attr"`
+    // xmlnsX      string       `xml"xmlns:x,attr"`
+    // xmlnsW      string       `xml"xmlns:w,attr"`
+    // xmlnsRsp    string       `xml"xmlns:rsp,attr"`
+    // xmlnsP      string       `xml"xmlns:p,attr"`
+    // xmlnsLang   string       `xml"xmlns:lang,attr"`
     Header      *ResponseHeader  `xml"s:Header"`
     Body        *ResponseBody    `xml"s:Body"`
 }
-
 
 func GetObjectFromXML(XMLinput io.Reader) (ResponseEnvelope, error) {    
     b, err := ioutil.ReadAll(XMLinput)
@@ -73,4 +98,51 @@ func GetObjectFromXML(XMLinput io.Reader) (ResponseEnvelope, error) {
     xml.Unmarshal(b, &response)
     return response, nil
     }
+}
+
+func GetCommandStdout(stream []ResponseStream) []byte{
+    var str bytes.Buffer
+    for _, value := range stream {
+        if value.End == "" && value.Name == "stdout"{
+            str.WriteString(value.Value)
+        }else{
+            break
+        }
+    }
+        s := str.String()
+        data, err := base64.StdEncoding.DecodeString(s)
+        if err != nil{
+            fmt.Println(err)
+    }
+    return data
+}
+
+func GetCommandStderr(stream []ResponseStream) []byte{
+    var str bytes.Buffer
+    for _, value := range stream {
+        if value.End == "" && value.Name == "stderr"{
+            str.WriteString(value.Value)
+        }else{
+            break
+        }
+    }
+        s := str.String()
+        data, err := base64.StdEncoding.DecodeString(s)
+        if err != nil{
+            fmt.Println(err)
+    }
+    return data
+}
+
+func GetCommandOutput(XMLinput io.Reader) (stdout, stderr string, exitcode int){
+    object, err := GetObjectFromXML(XMLinput)
+    if err != nil{
+        fmt.Println(err)
+    }
+    stdout_array := GetCommandStdout(object.Body.ReceiveResponse.Stream)
+    stderr_array := GetCommandStderr(object.Body.ReceiveResponse.Stream)
+    exitcode = object.Body.ReceiveResponse.CommandState.ExitCode
+    stdout = string(stdout_array[:])
+    stderr = string(stderr_array[:])
+    return stdout, stderr, exitcode
 }
